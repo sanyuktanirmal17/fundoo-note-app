8cd6d3be9fff7b234ad70df4e1a20afa1b5e89a1
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-08-05 23:02:34 +0530

    refactor:  Testcase added

--- test/login-test.js
+++ test/login-test.js
@@ -1,59 +1,59 @@
-// const chai = require('chai')
-// const chaiHttp = require('chai-http')
-// const server = require('../server')
-// const userdata= require('./data.json')
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const server = require('../server')
+const userdata= require('./data.json')
 
-// chai.should ()
-// chai.use(chaiHttp)
+chai.should ()
+chai.use(chaiHttp)
 
-//     /* 
-//     POST API test for login
-//     */
-//     describe('POST ./login', () => {
-//         it(" post_login_details", (done) => {
-//             const loginData = userdata.data.login
-//              chai.request(server)
-//                 .post('/login')
-//                 .send(loginData)
-//                 .end((error, res) => {
-//                     res.should.have.status(200)
-//                 done()
-//             })
-//         }) 
+    /* 
+    POST API test for login
+    */
+    describe('POST ./login', () => {
+        it(" post_login_details", (done) => {
+            const loginData = userdata.data.login
+             chai.request(server)
+                .post('/login')
+                .send(loginData)
+                .end((error, res) => {
+                    res.should.have.status(200)
+                done()
+            })
+        }) 
   
 
-//     /* 
-//     POST API negative test for login
-//     */
-//     // it("It_should_not_post_login_details", (done) => {
-//     //     const loginDataNegative = userdata.data.loginNegative
-//     //     chai.request(server)
-//     //         .post("/login")
-//     //         .send(loginDataNegative)
-//     //         .end((error, res) => {
-//     //          console.log(res);
-//     //         done()
-//     //     })
-//     //  })
-//     it("givenLoginDetails_whenImproper_impropperPassword", (done) => {
-//         const loginData = userdata.data.loginWithImproperepassword
-//         chai.request(server)
-//             .post("/login")
-//             .send(loginData)
-//             .end((error, res) => {
-//             res.should.have.status(400)
-//             done()
-//         })
-//      })
+    /* 
+    POST API negative test for login
+    */
+    // it("It_should_not_post_login_details", (done) => {
+    //     const loginDataNegative = userdata.data.loginNegative
+    //     chai.request(server)
+    //         .post("/login")
+    //         .send(loginDataNegative)
+    //         .end((error, res) => {
+    //          console.log(res);
+    //         done()
+    //     })
+    //  })
+    it("givenLoginDetails_whenImproper_impropperPassword", (done) => {
+        const loginData = userdata.data.loginWithImproperepassword
+        chai.request(server)
+            .post("/login")
+            .send(loginData)
+            .end((error, res) => {
+            res.should.have.status(400)
+            done()
+        })
+     })
 
-//      it("givenLoginDetails_whenImproper_impropperEmail", (done) => {
-//         const loginData = userdata.data.loginWithImproperEmail
-//         chai.request(server)
-//             .post("/login")
-//             .send(loginData)
-//             .end((error, res) => {
-//             res.should.have.status(400)
-//             done()
-//         })
-//      })
-// }) 
+     it("givenLoginDetails_whenImproper_impropperEmail", (done) => {
+        const loginData = userdata.data.loginWithImproperEmail
+        chai.request(server)
+            .post("/login")
+            .send(loginData)
+            .end((error, res) => {
+            res.should.have.status(400)
+            done()
+        })
+     })
+}) 
--- test/register-test.js
+++ test/register-test.js
@@ -10,33 +10,33 @@ chai.use(chaiHttp)
 /* 
  POST API test for registeration
     */
-    // describe('POST /register', () => {
-    //     it("It_should_post_register_details", (done) => {
-    //         const registerData = userdata.data.register
-    //         chai.request(server)
-    //             .post("/register")
-    //             .send(registerData)
-    //             .end((error, res) => {
-    //                 res.should.have.status(200);
-    //             done()
-    //         })
-    //     }) 
+    describe('POST /register', () => {
+        it("It_should_post_register_details", (done) => {
+            const registerData = userdata.data.register
+            chai.request(server)
+                .post("/register")
+                .send(registerData)
+                .end((error, res) => {
+                    res.should.have.status(200);
+                done()
+            })
+        }) 
     
     
 
 
-        // it("Given_valid_details_except_firstname_It_should_not_post_register_details", (done) => {
-        //     const DataWithoutFirstName = userdata.data.WithoutFirstName
-        //     chai.request(server)
-        //         .post("/register")
-        //         .send(DataWithoutFirstName)
-        //         .end((error, res) => {
-        //          res.should.have.status(400);
-        //          res.body.should.be.a('object')
-        //          res.body.should.have.property("message").eql('\"firstName\" is not allowed to be empty')
-        //         done()
-        //     })
-        // })
+        it("Given_valid_details_except_firstname_It_should_not_post_register_details", (done) => {
+            const DataWithoutFirstName = userdata.data.WithoutFirstName
+            chai.request(server)
+                .post("/register")
+                .send(DataWithoutFirstName)
+                .end((error, res) => {
+                 res.should.have.status(400);
+                 res.body.should.be.a('object')
+                 res.body.should.have.property("message").eql('\"firstName\" is not allowed to be empty')
+                done()
+            })
+        })
     
        it("Given_valid_details_except_lastname_It_should_not_post_register_details", (done) => {
             const DataWithoutLastName = userdata.WithoutLastName
@@ -48,4 +48,4 @@ chai.use(chaiHttp)
                 done()
             })
         }) 
-    
+    })
